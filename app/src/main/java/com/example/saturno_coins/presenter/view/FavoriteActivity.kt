40cdc49e264ed80026104ce9f32b89fa26e24779
guinther-erich <guinther.erich@gmail.com>
2022-06-01 14:49:14 -0300

    Implementação da favorite activity

--- app/src/main/java/com/example/saturno_coins/presenter/view/FavoriteActivity.kt
+++ app/src/main/java/com/example/saturno_coins/presenter/view/FavoriteActivity.kt
@@ -1,4 +1,25 @@
 package com.example.saturno_coins.presenter.view
 
-class FavoriteActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.saturno_coins.databinding.ActivityCoinFavoritesBinding
+
+class FavoriteActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityCoinFavoritesBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        goToCoinList()
+    }
+
+    private fun goToCoinList() {
+        binding.buttonMain.setOnClickListener {
+            val coinDetailsActivity = Intent(this, CoinDetailsActivity::class.java)
+            startActivity(coinDetailsActivity)
+        }
+    }
 }
